483098399be3b1fe7fab6584c68dbe1443c817a5
GiovannyOtarola <giovannyxdd@gmail.com>
2024-10-26 17:00:31 -0300

    Initial commit

---
+++ src/main/java/com/example/sumativafs3b/Sumativafs3bApplication.java
@@ -0,0 +1,13 @@
+package com.example.sumativafs3b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sumativafs3bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sumativafs3bApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sumativafs3b/Sumativafs3bApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sumativafs3b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sumativafs3bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
